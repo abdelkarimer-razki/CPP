26788063cdf10e07e40e675bb323bb4e29c5a7bd
Abdelkarim Er-razki <aer-razk@e1r12p3.1337.ma>
2022-10-14 15:23:47 +0100

    fixing makefiles in cpp01

---
+++ cpp03/ex00/main.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aer-razk <aer-razk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 14:07:27 by aer-razk          #+#    #+#             */
+/*   Updated: 2022/10/14 15:10:16 by aer-razk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main()
+{
+	ClapTrap a("karim");
+	a.attack("hamid");
+a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");a.attack("hamid");
+}
